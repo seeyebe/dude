c3a86c210239563b540412dc32609c855d37c1da
Dave <17879596+daved09@users.noreply.github.com>
2025-08-06 10:50:40 +0200

    excluding explicitly installed and optional dependencies (#1)

--- src/core/alpm.rs
+++ src/core/alpm.rs
@@ -11,13 +11,14 @@ impl AlpmContext {
         Ok(Self { alpm: Alpm::new("/", "/var/lib/pacman/")? })
     }
 
-    /// True orphans: installed as dependencies and required_by list is empty.
+    /// True orphans: installed as dependencies, required_by list is empty and not explicitly installed.
     pub fn get_orphans(&self) -> Result<Vec<Package>> {
         let pkgs = self.alpm.localdb().pkgs();
 
         let mut orphans: Vec<Package> = pkgs
             .iter()
-            .filter(|p| p.reason() == PackageReason::Depend && p.required_by().is_empty())
+            .filter(|p| p.reason() == PackageReason::Depend && p.reason() != PackageReason::Explicit && 
+            p.required_by().is_empty() && p.optional_for().is_empty())
             .filter_map(|p| Package::from_alpm_package(p).ok())
             .collect();
 
